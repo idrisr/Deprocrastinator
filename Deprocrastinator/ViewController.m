a8ffd74e2355e3a33cfa779c8cabf1fe4716e71e
Idris Raja <idris.raja@gmail.com>
2016-03-21 14:59:39 -0500

    change value to green or delete based on whether were editing

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -13,7 +13,7 @@
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property NSMutableArray *toDoItems;
 @property NSMutableArray *colors;
-
+@property BOOL isEditing;
 @end
 
 @implementation ViewController
@@ -21,6 +21,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.toDoItems = [[NSMutableArray alloc] init];
+    self.colors = [[NSMutableArray alloc] init];
+    self.isEditing = NO;
+
     // TODO: make me pretty
     self.inputTextField.layer.borderColor = [UIColor blackColor].CGColor;
     self.inputTextField.layer.borderWidth = 2.0f;
@@ -33,19 +36,39 @@
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellID"];
     cell.textLabel.text = [self.toDoItems objectAtIndex:indexPath.row];
+    cell.textLabel.textColor = [self.colors objectAtIndex:indexPath.row];
     return cell;
 }
 
 - (IBAction)addButtonPressed:(UIBarButtonItem *)sender {
     NSString *task = self.inputTextField.text;
     [self.toDoItems addObject:task];
+    [self.colors addObject:[UIColor blackColor]];
+
     [self.tableView reloadData];
     self.inputTextField.text = @"";
     [self.inputTextField resignFirstResponder];
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    if (self.isEditing) {
+        [self.colors removeObjectAtIndex:indexPath.row];
+        [self.toDoItems removeObjectAtIndex:indexPath.row];
+    } else {
+        self.colors[indexPath.row] = [UIColor greenColor];
+        [self.tableView reloadData];
+    }
+    [self.tableView reloadData];
 }
+
+- (IBAction)onEditButtonPressed:(UIBarButtonItem *)sender {
+    self.isEditing = !self.isEditing;
+    if (self.isEditing) {
+        sender.title = @"Done";
+    } else {
+        sender.title = @"Edit";
+    }
+}
+
 
 @end
